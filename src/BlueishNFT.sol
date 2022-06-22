940f226ef992ecdd79b6944c134be60fc4b8a488
Matthew Salerno <matthew@buoydevelopment.com>
2022-06-22 15:39:39 -0700

    Working through adding tests

--- src/BlueishNFT.sol
+++ src/BlueishNFT.sol
@@ -4,11 +4,8 @@ pragma solidity ^0.8.13;
 import "solmate/tokens/ERC721.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol"; 
 
-error MintPriceNotPaid();
-
 contract BlueishNFT is ERC721 {
-
-    uint256 public constant MINT_PRICE = 0.08 ether;
+    uint256 public currentTokenId;
 
     constructor(
         string memory _name,
@@ -16,10 +13,9 @@ contract BlueishNFT is ERC721 {
     ) ERC721(_name, _symbol) {}
 
     function mintTo(address recipient) public payable returns (uint256) {
-        if (msg.value != MINT_PRICE) {
-            revert MintPriceNotPaid();
-        }
-
+        uint256 newTokenId = ++currentTokenId;
+       _safeMint(recipient, newTokenId);
+       return newTokenId;
     }
 
     function tokenURI(uint256 id) public view virtual override returns (string memory) {
--- test/BlueishNFT.t.sol
+++ test/BlueishNFT.t.sol
@@ -5,6 +5,8 @@ import "forge-std/Test.sol";
 import "../src/BlueishNFT.sol";
 
 contract BlueishNFTTest is Test {
+    using stdStorage for StdStorage;
+
     BlueishNFT private blueishNFT;
 
     function setUp() public {
@@ -12,11 +14,56 @@ contract BlueishNFTTest is Test {
         blueishNFT = new BlueishNFT("Blueish NFT", "BLU");
     }
 
-    function testFailNoMintPricePaid() public {
-        blueishNFT.mintTo(address(1));
+
+    // tests beginning of 'testFail' in function signature will 'pass' if reverted and 'fail' if not reverted
+    // this is testing for vm/contract to revert. This is NOT the same as a failing test assertion and if you try to
+    // use testFail with failing assertions, and not reverts, you will get unexpected behavior, i.e. 'passing' tests when you are
+    // expecting them to fail
+    function testFailMintToZeroAddress() public {
+        blueishNFT.mintTo(address(0));
     }
 
-    function testMintTo() public {
-        assertTrue(true);
+    // tests beginning with 'test' in function signature are run as tests cases
+    function testBalanceOfIncremented() public {
+        blueishNFT.mintTo(address(1));
+
+        //stdstore gives us access to contract state/storage. You can append the following query functions to stdstore
+        //
+        //Query functions (configure the state query - this will return a storage slot):
+        // .target - address of contract whose storage you want to access (required)
+        // .sig - function signature that access storage (required) - format is contract.<functionName>.selector
+        // .with_key - if storage is mapping, set the mapping key
+        // .depth - index of struct member, if state var w e are accessing is type Struct
+        // Terminator functions (actions):
+        // .checked_write - set data to be written to storage slot
+        // .find() - return the slot numnber
+        // .read() - access value at the targeted storage slot
+        // .find() is helpful if you want to retain a pointer to a specific storage slot that can be accessed multiple times 
+        // in a test (for instance to check for changing state)
+        // vs. read(), which is helpful if the value being accessed is going to used or asserted. 
+        // This:
+        // uint256 slot = foo.find()
+        //uint256 value = uint256(vm.load(address(contract), bytes32(slot)))
+        // Is Equal to this:
+        // uint256 value = foo.read_int()
+
+        uint256 storageSlotForBalanceOf = stdstore
+            .target(address(blueishNFT))
+            .sig(blueishNFT.balanceOf.selector)
+            .with_key(1)
+            .find();
+
+        uint256 origBalance = uint256(
+            vm.load(address(blueishNFT), bytes32(storageSlotForBalanceOf))
+        );
+
+        assertEq(origBalance, 1);
+        blueishNFT.mintTo(address(1));
+
+        uint256 newBalance = uint256(
+            vm.load(address(blueishNFT), bytes32(storageSlotForBalanceOf))
+        );
+
+        assertEq(newBalance, 2);
     }
 }
