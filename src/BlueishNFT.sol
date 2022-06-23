5849ff2844251074bc2dd5b111c17d928aede5f8
Matthew Salerno <matthew@buoydevelopment.com>
2022-06-23 14:47:26 -0700

    SVG generation and encoding; Need to addd mockCalls to tests to test tokenURI interface

--- src/BlueishNFT.sol
+++ src/BlueishNFT.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import "solmate/tokens/ERC721.sol";
 import "openzeppelin-contracts/contracts/utils/Strings.sol"; 
+import "openzeppelin-contracts/contracts/utils/Base64.sol";
 
 contract BlueishNFT is ERC721 {
     uint256 public currentTokenId;
@@ -19,6 +20,20 @@ contract BlueishNFT is ERC721 {
     }
 
     function tokenURI(uint256 id) public view virtual override returns (string memory) {
+        foo(); 
+    }
 
+    function foo() public pure {
+
+    }
+
+    function svgForToken(uint256 tokenId) public pure returns(string memory svg) {
+       svg =  "<svg width='140' height='140'><rect x='0' y='0' width='140' height='140' style='fill:#ffffff;stroke-width:3;stroke:black'/></svg>";
+    }
+
+    function svgToImageURI(string memory svg) public pure returns(string memory imgURI) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory encodedSVG = Base64.encode(bytes(svg));
+        imgURI = string(abi.encodePacked(baseURL, encodedSVG));
     }
 }
--- test/BlueishNFT.t.sol
+++ test/BlueishNFT.t.sol
@@ -14,7 +14,6 @@ contract BlueishNFTTest is Test {
         blueishNFT = new BlueishNFT("Blueish NFT", "BLU");
     }
 
-
     // tests beginning of 'testFail' in function signature will 'pass' if reverted and 'fail' if not reverted
     // this is testing for vm/contract to revert. This is NOT the same as a failing test assertion and if you try to
     // use testFail with failing assertions, and not reverts, you will get unexpected behavior, i.e. 'passing' tests when you are
@@ -38,9 +37,9 @@ contract BlueishNFTTest is Test {
         // .checked_write - set data to be written to storage slot
         // .find() - return the slot numnber
         // .read() - access value at the targeted storage slot
-        // .find() is helpful if you want to retain a pointer to a specific storage slot that can be accessed multiple times 
+        // .find() is helpful if you want to retain a pointer to a specific storage slot that can be accessed multiple times
         // in a test (for instance to check for changing state)
-        // vs. read(), which is helpful if the value being accessed is going to used or asserted. 
+        // vs. read(), which is helpful if the value being accessed is going to used or asserted.
         // This:
         // uint256 slot = foo.find()
         //uint256 value = uint256(vm.load(address(contract), bytes32(slot)))
@@ -65,5 +64,50 @@ contract BlueishNFTTest is Test {
         );
 
         assertEq(newBalance, 2);
+    }
+
+    function testGenerateSVG() public {
+        uint256 tokenId = blueishNFT.mintTo(address(1));
+        string memory svg = blueishNFT.svgForToken(tokenId);
+        // keccak256(abi.encodePacked(string)) is used for string comparison
+        // the third param passed to Forge's std lib `assertEq` assertion is output in the case of a failng assertion
+        // This output will only show when running `forge test` with verbosity level 2, `forge test -vv`
+        string
+            memory testSVGMarkup = "<svg width='140' height='140'><rect x='0' y='0' width='140' height='140' style='fill:#ffffff;stroke-width:3;stroke:black'/></svg>";
+        assertEq(
+            keccak256(abi.encodePacked(testSVGMarkup)),
+            keccak256(abi.encodePacked(svg)),
+            "SVG output does not equal expected"
+        );
+    }
+
+    function testGenerateSVGForImageURI() public {
+        uint256 tokenId = blueishNFT.mintTo(address(1));
+
+        // set up expected value for base64 encoded SVG image URI
+        string memory svg = blueishNFT.svgForToken(tokenId);
+        string memory encodedSVG = Base64.encode(bytes(svg));
+        string memory svgDataURIbaseURL = "data:image/svg+xml;base64,";
+        string memory expectedImageURI = string(
+            abi.encodePacked(svgDataURIbaseURL, encodedSVG)
+        );
+
+        string memory base64encodedSVGURI = blueishNFT.svgToImageURI(svg);
+
+        assertEq(
+            keccak256(abi.encodePacked(expectedImageURI)),
+            keccak256(abi.encodePacked(base64encodedSVGURI)),
+            "Image URI output does not equal expected"
+        );
+    }
+
+    function testTokenURI() public {
+        uint256 tokenId = blueishNFT.mintTo(address(1));
+
+        // Unfortunately the below doesn't work with internal calls yet : https://github.com/foundry-rs/foundry/issues/876
+        
+        // vm.expectCall(address(blueishNFT), abi.encodeCall(blueishNFT.foo, ()));
+        // blueishNFT.tokenURI(tokenId);
+        // string memory tokenURI = blueishNFT.tokenURI(tokenId);
     }
 }
